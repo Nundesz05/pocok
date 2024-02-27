6e130b9985f7feeba2498e797824b28a141f4ff2
Nundesz05 <nundesz06@gmail.com>
2024-02-27 14:24:36 +0100

    Start

---
+++ src/main/java/com/example/pocok/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.pocok;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class HelloController {
+    @FXML private Label pocokDb;
+    @FXML private Label bujtkapottDb;
+    @FXML private Label arany;
+    @FXML private Button jatekButton;
+    @FXML private Pane Panel;
+
+
+}
